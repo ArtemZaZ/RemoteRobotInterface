9388f4bb442080f43cfd7d67f53277369bfb930e
ArtemZaZ <temka.911@mail.ru>
2018-10-30 17:02:19 +0300

    Обновил

--- udpcp.py
+++ udpcp.py
@@ -1,4 +1,5 @@
 import struct
+import pickle
 
 """
 UDP Control Protocol - UDP Протокол управления
@@ -11,12 +12,27 @@ UDP Control Protocol - UDP Протокол управления
     :field args(var): поле параметров ф-ии(следуют друг за другом)
 """
 
+_protocolHeadConfig = {
+    "start": b'\xAA\xAA',   # стартовые байты
+    "formatPackageLen": 'i',  # 4 байта
+    "formatPackageNum": 'i',  # 4 байта
+    "formatChecksum": 'i',    # 4 байта
+    "checksum": "crc8"      # хеш сумма crc8
+}
 
-def pack(functionName, args, argsTypes, packageNumber, checksum):
-    packageFormat = "iii%dsc%dsc" % (len(argsTypes), len(functionName)) + argsTypes
-    return struct.pack(packageFormat, struct.calcsize(packageFormat), checksum, packageNumber,
-                       bytes(bytearray(argsTypes, "utf-8")), b'/',
-                       bytes(bytearray(functionName, "utf-8")), b'/', *args)
+_headFormat = _protocolHeadConfig["formatPackageLen"]\
+            + _protocolHeadConfig["formatPackageNum"]\
+            + _protocolHeadConfig["formatChecksum"]
+
+_headSize = len(_protocolHeadConfig["start"]) + struct.calcsize(_headFormat)
+
+
+def pack(packageNumber, functionName, *args):
+    data = pickle.dumps((functionName, *args), 3)
+    packageLen = len(data) + _headSize
+    checksum = 0
+    head = _protocolHeadConfig["start"] + struct.pack(_headFormat, packageLen, checksum, packageNumber)
+    return head + data
 
 
 def unpack(package):
@@ -24,5 +40,5 @@ def unpack(package):
 
 
 if __name__ == "__main__":
-    package = pack("move", (1, 2.0, b's'), "ifc", 0, 16)
-    print(unpack(package))
+    package = pack(10, "move", "ifc", 0, 16)
+    print(package)
