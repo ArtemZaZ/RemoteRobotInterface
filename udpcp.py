1aa47dc856771b6e5988b3ecb9fef830d59922be
ArtemZaZ <temka.911@mail.ru>
2018-10-31 17:23:43 +0300

    Доделал первую версию протокола

--- udpcp.py
+++ udpcp.py
@@ -1,13 +1,13 @@
 import struct
 import pickle
+import crc16
 
 """
 UDP Control Protocol - UDP Протокол управления
-    :package format: <<package len | checksum | package number | args types*\* | function name*\* | args >>
+    :package format: <<package len | checksum | package number ||| function name | args >>
     :field package len(4 байта): длина пакета в байтах
-    :field checksum(4 байта): избыточный код для проверки сообщения на ошибки
+    :field checksum(2 байта): избыточный код для проверки сообщения на ошибки
     :field package number(4 байта): номер пакета(каждый новый пакет +1 к номеру, после переполнения 4 байт обнуляется)
-    :field args types(var): типы аргументов ф-ий(следуют друг за другом(прим. ffif)). *\* - разделитель сообщения
     :field function name(var): имя ф-ии, которая вызовется на роботе
     :field args(var): поле параметров ф-ии(следуют друг за другом)
 """
@@ -16,8 +16,8 @@ _protocolHeadConfig = {
     "start": b'\xAA\xAA',   # стартовые байты
     "formatPackageLen": 'i',  # 4 байта
     "formatPackageNum": 'i',  # 4 байта
-    "formatChecksum": 'i',    # 4 байта
-    "checksum": "crc8"      # хеш сумма crc8
+    "formatChecksum": 'H',    # 2 байта
+    "checksum": "crc16"      # хеш сумма crc16
 }
 
 _headFormat = _protocolHeadConfig["formatPackageLen"]\
@@ -28,17 +28,32 @@ _headSize = len(_protocolHeadConfig["start"]) + struct.calcsize(_headFormat)
 
 
 def pack(packageNumber, functionName, *args):
-    data = pickle.dumps((functionName, *args), 3)
-    packageLen = len(data) + _headSize
-    checksum = 0
-    head = _protocolHeadConfig["start"] + struct.pack(_headFormat, packageLen, checksum, packageNumber)
+    """ Упаковка пакета данных в пакет UDPCP """
+    data = pickle.dumps((functionName, *args), 3)   # сами данные
+    packageLen = len(data) + _headSize  # суммарная длина пакета
+    checksum = crc16.crc16xmodem(data)  # контрольная сумма
+    head = _protocolHeadConfig["start"] + struct.pack(_headFormat, packageLen, checksum, packageNumber)     # заголовок
     return head + data
 
 
 def unpack(package):
-    return struct.unpack("iii", package[0:12])
+    """ Распаковка пакета UDPCP """
+    head = struct.unpack(_headFormat, package[len(_protocolHeadConfig["start"]):_headSize])     # кортеж заголовка
+    data = package[_headSize:]  # сырые данные
+    return (*head, *(pickle.loads(data)))   # распаковываем кортежи и запаковываем в кортеж
+
+
+def check(package):
+    """ Проверка контрольной суммы """
+    _, checksum, _ = struct.unpack(_headFormat, package[len(_protocolHeadConfig["start"]):_headSize])   # получаем
+    #  контрольную сумму
+    data = package[_headSize:]  # сырые данные
+    hesh = crc16.crc16xmodem(data)  # считаем контрольную сумму
+    return hesh == checksum     # возвращаем результат
 
 
 if __name__ == "__main__":
     package = pack(10, "move", "ifc", 0, 16)
-    print(package)
+    if check(package):
+        data = unpack(package)
+        print(data)
